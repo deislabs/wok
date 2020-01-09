a2e510817c7efb63a7b609212893d5067cf07584
Matt Butcher <matt.butcher@microsoft.com>
2020-01-09 13:53:51 -0700

    add test for version
    Signed-off-by: Matt Butcher <matt.butcher@microsoft.com>

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,9 @@ pub mod grpc {
 /// The version of the runtime API that this tool knows.
 /// See CRI-O for reference (since docs don't explain this)
 /// https://github.com/cri-o/cri-o/blob/master/server/version.go
-const RUNTIME_API_VERSION: &str = "0.1.0";
+const RUNTIME_API_VERSION: &str = "v1alpha2";
+/// The API version of this CRI plugin.
+const API_VERSION: &str = "0.1.0";
 
 type CriResult<T> = std::result::Result<Response<T>, Status>;
 
@@ -30,12 +32,42 @@ impl RuntimeService for CRIRuntimeService {
     async fn version(&self, req: Request<VersionRequest>) -> CriResult<VersionResponse> {
         log::info!("Version request from API version {:?}", req);
         Ok(Response::new(VersionResponse {
-            version: RUNTIME_API_VERSION.to_string(),
+            version: API_VERSION.to_string(),
             runtime_name: env!("CARGO_PKG_NAME").to_string(),
             runtime_version: env!("CARGO_PKG_VERSION").to_string(),
             // NOTE: The Kubernetes API distinctly says that this MUST be a SemVer...
             // but actually require this format, which is not SemVer at all.
-            runtime_api_version: "v1alpha2".to_string(),
+            runtime_api_version: RUNTIME_API_VERSION.to_string(),
         }))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::grpc::VersionRequest;
+    use futures::executor::block_on;
+    use tonic::Request;
+    #[test]
+    fn test_version() {
+        block_on(_test_version())
+    }
+
+    async fn _test_version() {
+        let svc = CRIRuntimeService {};
+        let res = svc.version(Request::new(VersionRequest::default())).await;
+        assert_eq!(
+            res.as_ref()
+                .expect("successful version request")
+                .get_ref()
+                .version,
+            API_VERSION
+        );
+        assert_eq!(
+            res.expect("successful version request")
+                .get_ref()
+                .runtime_api_version,
+            RUNTIME_API_VERSION
+        );
     }
 }
