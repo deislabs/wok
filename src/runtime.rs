dca399d758be736a2880b86c3d2f30a454219b6b
Taylor Thomas <taylor.thomas@microsoft.com>
2020-01-09 17:07:27 -0700

    WIP: run_pod_sandbox

--- src/runtime.rs
+++ src/runtime.rs
@@ -1,8 +1,11 @@
-use tonic::{Request, Response};
+use tonic::{Request, Response, Status};
 
 // The RuntimeService grpc is converted to a package runtime_service_server
-use crate::grpc::{runtime_service_server::RuntimeService, VersionRequest, VersionResponse};
-use crate::CriResult;
+use crate::grpc::{
+    runtime_service_server::RuntimeService, RunPodSandboxRequest, RunPodSandboxResponse,
+    VersionRequest, VersionResponse,
+};
+use crate::{CriResult, Result};
 
 /// The version of the runtime API that this tool knows.
 /// See CRI-O for reference (since docs don't explain this)
@@ -12,6 +15,29 @@ pub const RUNTIME_API_VERSION: &str = "0.1.0";
 /// Implement a CRI runtime service.
 #[derive(Debug, Default)]
 pub struct CRIRuntimeService {}
+
+pub enum RuntimeHandler {
+    WASI,
+    WASCC,
+}
+
+impl RuntimeHandler {
+    pub fn from_string(s: &str) -> Result<Self> {
+        match s {
+            // Per the spec, the empty string should use the default
+            "" => Ok(Self::default()),
+            "WASI" => Ok(Self::WASI),
+            "WASCC" => Ok(Self::WASCC),
+            _ => Err(format_err!("Invalid runtime handler {}", s)),
+        }
+    }
+}
+
+impl Default for RuntimeHandler {
+    fn default() -> Self {
+        Self::WASI
+    }
+}
 
 #[tonic::async_trait]
 impl RuntimeService for CRIRuntimeService {
@@ -24,6 +50,20 @@ impl RuntimeService for CRIRuntimeService {
             // NOTE: The Kubernetes API distinctly says that this MUST be a SemVer...
             // but actually require this format, which is not SemVer at all.
             runtime_api_version: "v1alpha2".to_string(),
+        }))
+    }
+
+    async fn run_pod_sandbox(
+        &self,
+        req: Request<RunPodSandboxRequest>,
+    ) -> CriResult<RunPodSandboxResponse> {
+        let sandbox_req = req.into_inner();
+        let handler = RuntimeHandler::from_string(&sandbox_req.runtime_handler)
+            .map_err(|e| Status::invalid_argument(e.to_string()))?;
+        // NOTE: As of now, there isn't networking support in wasmtime, so we
+        // can't necessarily set it up right now
+        Ok(Response::new(RunPodSandboxResponse {
+            pod_sandbox_id: String::default(),
         }))
     }
 }
