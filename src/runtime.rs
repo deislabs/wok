964fcc94685608494cd0e787ae3d93c10f80c6b5
Taylor Thomas <taylor.thomas@microsoft.com>
2020-01-14 20:03:29 -0700

    feat(runtime): Adds run pod sandbox implementation
    This takes a "get it up and running" approach to things. Currently we
    are storing all information in memory protected by a read/write lock.
    There are several optimizations I am sure could be done in the future.
    Also, I could be doing some Rust things wrong, so feel free to teach me.

--- src/runtime.rs
+++ src/runtime.rs
@@ -1,23 +1,20 @@
-use std::collections::HashMap;
+use std::collections::{BTreeMap, HashMap};
+use std::sync::{Arc, RwLock};
 use tonic::{Request, Response, Status};
 // RuntimeService is converted to a package runtime_service_server
 use crate::grpc::{
-    runtime_service_server::RuntimeService,
+    runtime_service_server::RuntimeService, Container, ContainerMetadata, ContainerStatus,
+    ContainerStatusRequest, ContainerStatusResponse, CreateContainerRequest,
+    CreateContainerResponse, ImageSpec, ListContainersRequest, ListContainersResponse,
+    ListPodSandboxRequest, ListPodSandboxResponse, PodSandbox, PodSandboxState,
+    PodSandboxStatusRequest, PodSandboxStatusResponse, RemoveContainerRequest,
+    RemoveContainerResponse, RemovePodSandboxRequest, RemovePodSandboxResponse,
+    RunPodSandboxRequest, RunPodSandboxResponse, StartContainerRequest, StartContainerResponse,
+    StopContainerRequest, StopContainerResponse, StopPodSandboxRequest, StopPodSandboxResponse,
     VersionRequest, VersionResponse,
-    PodSandbox,
-    ListPodSandboxRequest, ListPodSandboxResponse,
-    PodSandboxStatusRequest, PodSandboxStatusResponse,
-    RemovePodSandboxRequest, RemovePodSandboxResponse,
-    RunPodSandboxRequest, RunPodSandboxResponse,
-    StopPodSandboxRequest, StopPodSandboxResponse,
-    Container, ContainerStatus, ContainerMetadata, ImageSpec,
-    CreateContainerRequest, CreateContainerResponse,
-    StartContainerRequest, StartContainerResponse,
-    StopContainerRequest, StopContainerResponse,
-    RemoveContainerRequest, RemoveContainerResponse,
-    ListContainersRequest, ListContainersResponse,
-    ContainerStatusRequest, ContainerStatusResponse,
 };
+use chrono::Utc;
+use uuid::Uuid;
 
 /// The version of the runtime API that this tool knows.
 /// See CRI-O for reference (since docs don't explain this)
@@ -35,16 +32,50 @@ pub type Result<T> = std::result::Result<T, failure::Error>;
 /// Implement a CRI runtime service.
 #[derive(Debug, Default)]
 pub struct CriRuntimeService {
-    pods: Vec<PodSandbox>,
+    // NOTE: we could replace this with evmap or crossbeam
+    sandboxes: Arc<RwLock<BTreeMap<String, PodSandbox>>>,
     containers: Vec<Container>,
 }
 
 impl CriRuntimeService {
     pub fn new() -> Self {
         CriRuntimeService {
-            pods: vec![],
+            sandboxes: Arc::new(RwLock::new(BTreeMap::default())),
             containers: vec![],
         }
+    }
+}
+
+#[derive(Debug)]
+pub enum RuntimeHandler {
+    WASI,
+    WASCC,
+}
+
+impl ToString for RuntimeHandler {
+    fn to_string(&self) -> String {
+        match self {
+            Self::WASI => "WASI".to_owned(),
+            Self::WASCC => "WASCC".to_owned(),
+        }
+    }
+}
+
+impl RuntimeHandler {
+    pub fn from_string(s: &str) -> Result<Self> {
+        match s {
+            // Per the spec, the empty string should use the default
+            "" => Ok(Self::default()),
+            "WASI" => Ok(Self::WASI),
+            "WASCC" => Ok(Self::WASCC),
+            _ => Err(format_err!("Invalid runtime handler {}", s)),
+        }
+    }
+}
+
+impl Default for RuntimeHandler {
+    fn default() -> Self {
+        Self::WASI
     }
 }
 
@@ -62,21 +93,69 @@ impl RuntimeService for CriRuntimeService {
         }))
     }
 
+    async fn run_pod_sandbox(
+        &self,
+        req: Request<RunPodSandboxRequest>,
+    ) -> CriResult<RunPodSandboxResponse> {
+        let sandbox_req = req.into_inner();
+        let sandbox_conf = match sandbox_req.config {
+            Some(c) => c,
+            None => {
+                return Err(Status::invalid_argument(
+                    "Sandbox request is missing config object",
+                ))
+            }
+        };
+        let handler = RuntimeHandler::from_string(&sandbox_req.runtime_handler)
+            .map_err(|e| Status::invalid_argument(e.to_string()))?;
+
+        // TODO(taylor): As of now, there isn't networking support in wasmtime,
+        // so we can't necessarily set it up right now. Once it does, we'll need
+        // to set up networking here
+
+        // Create the logs directory for this pod
+        std::fs::create_dir_all(sandbox_conf.log_directory.clone())
+            .map_err(|e| Status::invalid_argument(e.to_string()))?;
+        // Basically, everything above here is all we need to set up a sandbox.
+        // All of the security context stuff pretty much doesn't matter for
+        // WASM, but we can revisit this as things keep evolving
+
+        // Ok, now let's store things in memory
+        let sandbox_map = self.sandboxes.clone();
+        // TODO(taylor): According to the RwLock docs, we should panic on failure
+        // to poison the RwLock. This also means we'd need to have handling for
+        // recovering from a poisoned RwLock, which I am leaving for later
+        let mut sandboxes = sandbox_map
+            .write()
+            .map_err(|e| {
+                Status::internal(format!(
+                    "Data inconsistency when trying to store sandbox data: {}",
+                    e.to_string()
+                ))
+            })
+            .unwrap();
+        let id = Uuid::new_v4().to_string();
+        sandboxes.insert(
+            id.clone(),
+            PodSandbox {
+                id: id.clone(),
+                metadata: sandbox_conf.metadata,
+                state: PodSandboxState::SandboxReady as i32,
+                created_at: Utc::now().timestamp_nanos(),
+                labels: sandbox_conf.labels,
+                annotations: sandbox_conf.annotations,
+                runtime_handler: handler.to_string(),
+            },
+        );
+        Ok(Response::new(RunPodSandboxResponse { pod_sandbox_id: id }))
+    }
+
     async fn list_pod_sandbox(
         &self,
         _req: Request<ListPodSandboxRequest>,
     ) -> CriResult<ListPodSandboxResponse> {
         Ok(Response::new(ListPodSandboxResponse {
-            items: self.pods.clone(),
-        }))
-    }
-
-    async fn run_pod_sandbox(
-        &self,
-        _req: Request<RunPodSandboxRequest>,
-    ) -> CriResult<RunPodSandboxResponse> {
-        Ok(Response::new(RunPodSandboxResponse {
-            pod_sandbox_id: "1".to_owned(),
+            items: self.sandboxes.read().unwrap().values().cloned().collect(),
         }))
     }
 
@@ -180,6 +259,7 @@ mod test {
     use super::*;
     use crate::grpc::*;
     use futures::executor::block_on;
+    use tempfile::tempdir;
     use tonic::Request;
 
     #[test]
@@ -190,6 +270,11 @@ mod test {
     #[test]
     fn test_run_pod_sandbox() {
         block_on(_test_run_pod_sandbox())
+    }
+
+    #[test]
+    fn test_create_and_list() {
+        block_on(_test_create_and_list())
     }
 
     #[test]
@@ -260,18 +345,6 @@ mod test {
         );
     }
 
-    async fn _test_run_pod_sandbox() {
-        let svc = CriRuntimeService::new();
-        let req = Request::new(RunPodSandboxRequest::default());
-        let res = svc.run_pod_sandbox(req).await;
-        assert_eq!(
-            "1".to_owned(),
-            res.expect("successful pod run submission")
-                .get_ref()
-                .pod_sandbox_id
-        );
-    }
-
     async fn _test_list_pod_sandbox() {
         let svc = CriRuntimeService::new();
         let req = Request::new(ListPodSandboxRequest::default());
@@ -309,7 +382,12 @@ mod test {
         let svc = CriRuntimeService::new();
         let req = Request::new(CreateContainerRequest::default());
         let res = svc.create_container(req).await;
-        assert_eq!("1", res.expect("successful create container").get_ref().container_id);
+        assert_eq!(
+            "1",
+            res.expect("successful create container")
+                .get_ref()
+                .container_id
+        );
     }
 
     async fn _test_start_container() {
@@ -340,13 +418,69 @@ mod test {
         let svc = CriRuntimeService::new();
         let req = Request::new(ListContainersRequest::default());
         let res = svc.list_containers(req).await;
-        assert_eq!(0, res.expect("successful list containers").get_ref().containers.len());
+        assert_eq!(
+            0,
+            res.expect("successful list containers")
+                .get_ref()
+                .containers
+                .len()
+        );
     }
 
     async fn _test_container_status() {
         let svc = CriRuntimeService::new();
         let req = Request::new(ContainerStatusRequest::default());
         let res = svc.container_status(req).await;
-        assert_eq!("because I said so", res.expect("successful container status").into_inner().status.unwrap().reason);
+        assert_eq!(
+            "because I said so",
+            res.expect("successful container status")
+                .into_inner()
+                .status
+                .unwrap()
+                .reason
+        );
+    }
+
+    async fn _test_run_pod_sandbox() {
+        let svc = CriRuntimeService::new();
+        let mut sandbox_req = RunPodSandboxRequest::default();
+        sandbox_req.runtime_handler = RuntimeHandler::WASI.to_string();
+
+        // Create a temporary log dir for testing purposes
+        let dir = tempdir().unwrap();
+        let log_dir_name = dir.path().join("testdir");
+        let mut conf = PodSandboxConfig::default();
+        conf.log_directory = log_dir_name.to_str().unwrap().to_owned();
+        sandbox_req.config = Some(conf);
+        let req = Request::new(sandbox_req);
+        let res = svc.run_pod_sandbox(req).await;
+        // Make sure we receive back a valid UUID
+        uuid::Uuid::parse_str(&res.unwrap().get_ref().pod_sandbox_id).unwrap();
+
+        // Now check that the log directory was created
+        assert_eq!(true, log_dir_name.exists());
+    }
+
+    async fn _test_create_and_list() {
+        let svc = CriRuntimeService::new();
+        let mut sandbox_req = RunPodSandboxRequest::default();
+        sandbox_req.runtime_handler = RuntimeHandler::WASI.to_string();
+
+        // Create a temporary log dir for testing purposes
+        let dir = tempdir().unwrap();
+        let log_dir_name = dir.path().join("testdir");
+        let mut conf = PodSandboxConfig::default();
+        conf.log_directory = log_dir_name.to_str().unwrap().to_owned();
+        sandbox_req.config = Some(conf);
+        let req = Request::new(sandbox_req);
+        let res = svc.run_pod_sandbox(req).await;
+
+        let id = res.unwrap().get_ref().pod_sandbox_id.clone();
+        let list_req = Request::new(ListPodSandboxRequest::default());
+        let res = svc.list_pod_sandbox(list_req).await;
+        let sandboxes = res.expect("successful pod list").get_ref().items.clone();
+        assert_eq!(1, sandboxes.len());
+        // And make sure the UID returned actually exists
+        assert_eq!(id, sandboxes[0].id);
     }
 }
