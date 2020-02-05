f5f8d78f1245553abf961ff517013357dc3a8833
Matthew Fisher <matt.fisher@microsoft.com>
2020-02-04 16:02:34 -0800

    cargo fmt
    Signed-off-by: Matthew Fisher <matt.fisher@microsoft.com>

--- src/runtime.rs
+++ src/runtime.rs
@@ -573,7 +573,9 @@ impl RuntimeService for CriRuntimeService {
     ) -> CriResult<StopContainerResponse> {
         let id = req.into_inner().container_id;
         let mut running_containers = self.running_containers.write().unwrap();
-        let runtime = running_containers.get_mut(&id).expect("cannot find container");
+        let runtime = running_containers
+            .get_mut(&id)
+            .expect("cannot find container");
         runtime.stop().expect("runtime can't stop");
         running_containers.remove(&id);
         Ok(Response::new(StopContainerResponse {}))
