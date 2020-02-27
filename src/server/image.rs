7791774bd17fc3aa0b6e98288df1c6366dda4683
Ryan Levick <rylev@users.noreply.github.com>
2020-02-27 11:00:36 +0100

    Scaffold CriImageService::image_status (#150)

--- src/server/image.rs
+++ src/server/image.rs
@@ -1,8 +1,9 @@
+use std::collections::HashMap;
 use std::convert::TryFrom;
 use std::path::PathBuf;
-use std::sync::Mutex;
 
 use chrono::Utc;
+use tokio::sync::Mutex;
 use tonic::{Request, Response};
 
 use super::grpc;
@@ -26,8 +27,8 @@ impl CriImageService {
         }
     }
 
-    fn pull_module(&self, module_ref: Reference) -> Result<(), failure::Error> {
-        self.module_store.lock().unwrap().pull(module_ref)?;
+    async fn pull_module<'a>(&self, module_ref: Reference<'a>) -> Result<(), failure::Error> {
+        self.module_store.lock().await.pull(module_ref)?;
         Ok(())
     }
 }
@@ -39,7 +40,30 @@ impl grpc::image_service_server::ImageService for CriImageService {
         _request: Request<grpc::ListImagesRequest>,
     ) -> CriResult<grpc::ListImagesResponse> {
         let resp = grpc::ListImagesResponse {
-            images: self.module_store.lock().unwrap().list().unwrap(),
+            images: self.module_store.lock().await.list().unwrap(),
+        };
+        Ok(Response::new(resp))
+    }
+
+    async fn image_status(
+        &self,
+        req: Request<grpc::ImageStatusRequest>,
+    ) -> CriResult<grpc::ImageStatusResponse> {
+        // TODO(rylev): handle error of image in request not being there.
+        let image_id = req.into_inner().image.unwrap().image;
+        let image = self
+            .module_store
+            .lock()
+            .await
+            .list()
+            .unwrap()
+            .iter()
+            .find(|i| i.id == image_id)
+            .cloned();
+
+        let resp = grpc::ImageStatusResponse {
+            image,
+            info: HashMap::new(), // TODO(rylev): figure out what goes here
         };
         Ok(Response::new(resp))
     }
@@ -49,8 +73,9 @@ impl grpc::image_service_server::ImageService for CriImageService {
         request: Request<grpc::PullImageRequest>,
     ) -> CriResult<grpc::PullImageResponse> {
         let image_ref = request.into_inner().image.unwrap().image;
-
-        self.pull_module(Reference::try_from(&image_ref).expect("Image ref is malformed"))
+        let reference = Reference::try_from(&image_ref).expect("Image ref is malformed");
+        self.pull_module(reference)
+            .await
             .expect("cannot pull module");
         let resp = grpc::PullImageResponse { image_ref };
 
@@ -64,7 +89,7 @@ impl grpc::image_service_server::ImageService for CriImageService {
         &self,
         _request: Request<grpc::ImageFsInfoRequest>,
     ) -> CriResult<grpc::ImageFsInfoResponse> {
-        let module_store = self.module_store.lock().unwrap();
+        let module_store = self.module_store.lock().await;
         let resp = grpc::ImageFsInfoResponse {
             image_filesystems: vec![grpc::FilesystemUsage {
                 timestamp: Utc::now().timestamp_nanos(),
@@ -85,5 +110,24 @@ impl grpc::image_service_server::ImageService for CriImageService {
             }],
         };
         Ok(Response::new(resp))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use grpc::image_service_server::ImageService;
+
+    #[tokio::test]
+    async fn test_image_status() {
+        let service = CriImageService::new(PathBuf::from(""));
+        let req = grpc::ImageStatusRequest {
+            image: Some(grpc::ImageSpec {
+                image: "foo/bar:baz".to_owned(),
+            }),
+            verbose: false,
+        };
+        let response = service.image_status(Request::new(req)).await;
+        assert!(response.unwrap().into_inner().image.is_none());
     }
 }
